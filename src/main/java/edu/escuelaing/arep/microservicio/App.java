a48e545490271702b5ec117a97e4dd2a47a6ff29
Jose1102 <jose.castroortega1102@gmail.com>
2021-08-11 21:42:40 -0500

    Add index and Procfile

--- src/main/java/edu/escuelaing/arep/microservicio/App.java
+++ src/main/java/edu/escuelaing/arep/microservicio/App.java
@@ -1,4 +1,8 @@
 package edu.escuelaing.arep.microservicio;
+import static spark.Spark.*;
+
+import spark.Request;
+import spark.Response;
 
 /**
  * Hello world!
@@ -6,8 +10,54 @@ package edu.escuelaing.arep.microservicio;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+	/**
+     * This main method uses SparkWeb static methods and lambda functions to
+     * create a simple Hello World web app. It maps the lambda function to the
+     * /hello relative URL.
+     */
+	public static void main(String[] args) {
+        //get("/hello", (req, res) -> "Hello World");
+        port(getPort());
+        staticFiles.location("/public");
+        get("/inputdata", (req, res) -> inputDataPage());
+        get("/results", (req, res) -> resultsPage(req, res));
     }
+
+    
+    
+   private static String inputDataPage() {
+        String pageContent
+                = "<!DOCTYPE html>"
+                + "<html>"
+                + "<body>"
+                + "<h2>HTML Forms</h2>"
+                + "<form action=\"/results\">"
+                + "  First name:<br>"
+                + "  <input type=\"text\" name=\"firstname\" value=\"Mickey\">"
+                + "  <br>"
+                + "  Last name:<br>"
+                + "  <input type=\"text\" name=\"lastname\" value=\"Mouse\">"
+                + "  <br><br>"
+                + "  <input type=\"submit\" value=\"Submit\">"
+                + "</form>"
+                + "<p>If you click the \"Submit\" button, the form-data will be sent to a page called \"/results\".</p>"
+                + "</body>"
+                + "</html>";
+        return pageContent;
+    }
+
+    private static String resultsPage(Request req, Response res) {
+        return req.queryParams("firstname") + " " +
+                req.queryParams("lastname");
+    }
+
+    
+    static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567; //returns default port if heroku-port isn't set (i.e. on localhost)
+    }
+    
+    
 }

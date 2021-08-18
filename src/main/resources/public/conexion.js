3c9560d3c50a17ce3963b579c4403fbef38c0432
Jose1102 <jose.castroortega1102@gmail.com>
2021-08-17 23:00:16 -0500

    add js

---
+++ src/main/resources/public/conexion.js
@@ -0,0 +1,20 @@
+const divRoot = document.querySelector('#root');
+const abb = <h1> Hola </h1>;
+const Api_key = 'OBTCETCUBVVUTSSL';
+
+
+const peticion = fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=fb&apikey=${ Api_key }`);
+
+peticion
+	.then( resp =>resp.json())
+	.then (datos => {
+		const valor = 'Meta Data';
+		const jsonString = datos[valor];
+		const respuesta = JSON.stringify(jsonString);
+		ReactDOM.render(respuesta, divRoot);
+		console.log(JSON.stringify(jsonString))
+	})
+	.catch(console.warn);
+
+	
+

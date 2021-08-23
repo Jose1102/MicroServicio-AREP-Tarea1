85ccc06d684c43c738577bfbed6d32a32da0f355
Jose1102 <jose.castroortega1102@gmail.com>
2021-08-23 17:07:47 -0500

    Add js and index with the identication

--- src/main/resources/public/conexion.js
+++ src/main/resources/public/conexion.js
@@ -1,21 +1,96 @@
-const divRoot = document.querySelector('#root');
-const abb = <h1> Hola </h1>;
-const Api_key = 'OBTCETCUBVVUTSSL';
+
+let symbolTemporal = getValueInput();
+function getValueInput() {
+
+	let inputValue = document.getElementById("domTextElement").value;
+	symbolTemporal = document.getElementById("domTextElement").value;
+
+	const divRoot = document.querySelector('#root');
+
+	const divTabla = document.querySelector('#tablaIdentificador');
+	const abb = <h1>Microservicio!</h1>
 
 
-const peticion = fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=fb&apikey=${ Api_key }`);
+	console.log('Entraaa ' + symbolTemporal)
 
-peticion
-	.then( resp =>resp.json())
-	.then (datos => {
-		const valor = 'Meta Data';
-		const jsonString = datos[valor];
-		const respuesta = JSON.stringify(jsonString);
-		ReactDOM.render(respuesta, divRoot);
-		console.log(datos);
-		console.log(JSON.stringify(jsonString))
-	})
-	.catch(console.warn);
 
-	
+	let Api_key = 'OBTCETCUBVVUTSSL';
+
+
+	let peticion = fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${symbolTemporal}&apikey=${Api_key}`);
+	//console.log(symbolTemporal);
+
+
+
+	peticion
+		.then(resp => resp.json())
+		.then(datos => {
+			const identi = 'Meta Data';
+
+
+			const information = '1. Information';
+			const symbol = '2. Symbol';
+			const refreshed = '3. Last Refreshed';
+			const outputSize = '4. Output Size';
+			const timeZone = '5. Time Zone';
+			const jsonString = datos[identi];
+			const tInformation = jsonString[information];
+			const tSymbol = jsonString[symbol];
+			const tRefreshed = jsonString[refreshed];
+			const tOutputSize = jsonString[outputSize];
+			const tTimeZone = jsonString[timeZone];
+
+
+			const respuesta = JSON.stringify(jsonString);
+			console.log(jsonString);
+			//console.log(datos);
+			console.log(jsonString[information])
+			//console.log(JSON.stringify(jsonString))
+
+			const tabla = <table class="table table-dark">
+				<thead>
+					<tr>
+						<th>Information</th>
+						<th>Symbol</th>
+						<th>Last Refreshed</th>
+						<th>Output Size</th>
+						<th>Time Zone</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>{tInformation}</td>
+						<td>{tSymbol}</td>
+						<td>{tRefreshed}</td>
+						<td>{tOutputSize}</td>
+						<td>{tTimeZone}</td>
+					</tr>
+				</tbody>
+			</table>;
+			ReactDOM.render(tabla, divTabla);
+
+
+
+
+
+		})
+		.catch(console.warn);
+
+
+
+
+
+	ReactDOM.render(abb, divRoot);
+
+
+}
+
+
+
+
+
+
+
+
+
 
